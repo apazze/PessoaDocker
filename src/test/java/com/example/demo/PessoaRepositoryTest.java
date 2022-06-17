aa352a822c4bc29e4c01d6aa81d2985abc71588e
Alisson Pazze <alisson_pazze@terceiros.sicredi.com.br>
2022-06-16 22:18:27 -0300

    @SpringBootTest usa o banco postgre da aplicação. @DataJpaTest dá rollback mas acaba incrementando as sequences. Com o testImplementation 'com.h2database:h2:2.1.214' no build.gradle os testes usarão o H2.

--- src/test/java/com/example/demo/PessoaRepositoryTest.java
+++ src/test/java/com/example/demo/PessoaRepositoryTest.java
@@ -1,17 +1,29 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 // com rollback
-/*@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@DataJpaTest*/
-@SpringBootTest
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@DataJpaTest
+
+// modifica no Banco da aplicação
+//@SpringBootTest
 class PessoaRepositoryTest {
     @Autowired
     PessoaRepository pessoaRepository;
+
+    @BeforeEach
+    void limpaBanco(){
+        pessoaRepository.deleteAll();
+    }
 
     @Test
     void deveInserir(){
@@ -20,9 +32,30 @@ class PessoaRepositoryTest {
         pessoaRepository.save(new Pessoa("bla"));
         pessoaRepository.save(new Pessoa("bla"));
 
-//        String nome = pessoaRepository.findById(2L).get().getNome();
+        List<Pessoa> lista = pessoaRepository.findAll();
+        Pessoa pessoa0 = lista.get(0);
+        Pessoa pessoa1 = lista.get(1);
+        Pessoa pessoa2 = lista.get(2);
+        Pessoa pessoa3 = lista.get(3);
 
-        //System.out.println("NOME ======== " + nome);
+        assertThat(pessoaRepository.findAll().size()).isEqualTo(4);
+        assertThat(pessoa0.getNome()).isEqualTo("bla");
+        assertThat(pessoa1.getNome()).isEqualTo("bla");
+        assertThat(pessoa2.getNome()).isEqualTo("bla");
+        assertThat(pessoa3.getNome()).isEqualTo("bla");
+
+    }
+
+    @Test
+    void deveExcluirTodosRegistros(){
+        pessoaRepository.deleteAll();
+        assertThat(pessoaRepository.findAll()).isEmpty();
+    }
+
+    @Test
+    void deveConterUmRegistro(){
+        pessoaRepository.save(new Pessoa("bla"));
+        assertThat(pessoaRepository.findAll().size()).isEqualTo(1);
     }
 
 }
